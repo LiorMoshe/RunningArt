0b00e83dc23d7b59010ba4b555c03a7b898e78cc
unknown <edrti@post.bgu.ac.il>
2020-03-30 20:31:23 +0300

    moosh

---
+++ osm/osm.py
@@ -0,0 +1,36 @@
+import overpy
+
+api = overpy.Overpass()
+
+# change <bbox-query> to the user's location bounding box
+result = api.query("""
+<query type="way" into="hw">
+  <has-kv k="highway"/>
+  <has-kv k="highway" modv="not" regv="footway|cycleway|path|service|track"/>
+   <bbox-query e="7.157" n="50.748" s="50.746" w="7.154"/>
+</query>
+
+<foreach from="hw" into="w">
+  <recurse from="w" type="way-node" into="ns"/>
+  <recurse from="ns" type="node-way" into="w2"/>
+  <query type="way" into="w2">
+    <item set="w2"/>
+    <has-kv k="highway"/>
+    <has-kv k="highway" modv="not" regv="footway|cycleway|path|service|track"/>
+  </query>
+  <difference into="wd">
+    <item set="w2"/>
+    <item set="w"/>
+  </difference>
+  <recurse from="wd" type="way-node" into="n2"/>
+  <recurse from="w"  type="way-node" into="n3"/>
+  <query type="node">
+    <item set="n2"/>
+    <item set="n3"/>
+  </query>
+  <print/>
+</foreach>
+    """)
+for node in result.nodes:
+    print(node)
+print(result.nodes)
