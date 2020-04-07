e70f885cc3030b249cc74ce4d762d42295fac3a7
LiorMoshe <liormoshe0909@gmail.com>
2020-04-07 21:57:55 +0300

    Updated rest server

--- rest-server.py
+++ rest-server.py
@@ -1,6 +1,7 @@
 import base64
 import io
 import traceback
+import math
 
 from PIL import Image
 from flask import Flask, jsonify, request, make_response
@@ -8,6 +9,45 @@ from flask_cors import CORS, cross_origin
 from flask_httpauth import HTTPBasicAuth
 
 from linedraw.draw import sketchImage
+
+def convert_coordinates(polylines, initial_pos, scale=1):
+    '''
+    TODO - Move this elsewhere.
+    Given the polylines given in the output of our algorithm and an initial geocentric location
+    on the map. Return a new set of polylines in geocentric location.
+    Apply scaling if necessary.
+    We use the fact that 111111 meters in the y direction is equivalent to 1 degree of latitude and
+    111111 * cos(latitude) meters in the x direction is 1 degree of longitude.
+    :param polylines The set of polylines given by the algorithm.
+    :param initial_pos Initial geo poistion in which we start our drawing.
+    :param scale The scale of the edges. TODO- Will be used in the future.
+    '''
+    if len(polylines) == 0 or len(polylines[0]) == 0:
+        raise ValueError("Received empty list of polylines")
+
+    flattened_polylines = []
+    for polyline in polylines:
+        for point in polyline:
+            flattened_polylines.append(point)
+
+    geo_polylines = [initial_pos]
+    current_geo = initial_pos
+    current_xy = flattened_polylines[0]
+    for idx, point in enumerate(flattened_polylines):
+        if idx > 0:
+            x_diff = point[0] - current_xy[0]
+            y_diff = -(point[1] - current_xy[1])
+
+            lat_diff = 1 / 111111 * y_diff
+            long_diff = 1 / (111111 * math.cos(current_geo[0] * math.pi / 180)) * x_diff
+
+            geo_pos = (current_geo[0] + lat_diff, current_geo[1] + long_diff)
+            geo_polylines.append(geo_pos)
+            current_geo = geo_pos
+            current_xy = point
+
+    return geo_polylines
+
 
 #example client request: curl -u running:art -F "file=@valtho.jpeg" -i http://localhost:5000/polylines
 
@@ -19,6 +59,8 @@ cors = CORS(app, resources={r"/polylines": {"origins": "http://localhost:port"}}
 auth = HTTPBasicAuth()
 
 IMAGE_KEY = 'Image'
+
+POSITION_KEY = 'Position'
 
 @auth.get_password
 def get_password(username):
@@ -45,11 +87,14 @@ def send_drawing():
     try:
         # Parse base64 image url.
         imageStr = request.json[IMAGE_KEY].split('base64,',1)[1]
+        initial_pos = request.json[POSITION_KEY]
+        print(initial_pos)
         msg = base64.b64decode(imageStr)
         buf = io.BytesIO(msg)
         img = Image.open(buf)
         lines = sketchImage(img)
-        return jsonify(lines)
+        geo_lines = convert_coordinates(lines, initial_pos)
+        return jsonify(geo_lines)
     except Exception:
         traceback.print_tb()
         return ""
