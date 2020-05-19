9a122cdc621feb4ca59c6478a6133b650e36b78e
edrti <edrti@post.bgu.ac.il>
2020-05-19 19:17:53 +0300

    zooz

--- osm/bounding_box_calculation.py
+++ osm/bounding_box_calculation.py
@@ -79,36 +79,6 @@ def get_intersection_nodes_idx(location_node, km_distance):
 </foreach>
     <print/>
     ''' % bbox
-    query = '''
-    <osm-script>
-<query type="way" into="hw">
-  <has-kv k="highway"/>
-  <has-kv k="highway" modv="not" regv="footway|cycleway|path|service|track"/>
-   <bbox-query e="34.773" n="32.060" s="32.057" w="34.768"/> 
-</query>
-
-<foreach from="hw" into="w">
-  <recurse from="w" type="way-node" into="ns"/>
-  <recurse from="ns" type="node-way" into="w2"/>
-  <query type="way" into="w2">
-    <item set="w2"/>
-    <has-kv k="highway"/>
-    <has-kv k="highway" modv="not" regv="footway|cycleway|path|service|track"/>
-  </query>
-  <difference into="wd">
-    <item set="w2"/>
-    <item set="w"/>
-  </difference>
-  <recurse from="wd" type="way-node" into="n2"/>
-  <recurse from="w"  type="way-node" into="n3"/>
-  <query type="node">
-    <item set="n2"/>
-    <item set="n3"/>
-  </query>
-  <print/>
-</foreach>
-  </osm-script>
-    '''
     result = api.query(query)
     return [node.id for node in result.nodes]
 
